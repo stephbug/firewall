c0ed5cd7efdccfce6a1cdf091ecfbe37b5a87354
steph bug <hewsda1@gmail.com>
2018-03-13 16:43:13 +0100

    implement http basic authentication

---
+++ src/Factory/Authentication/HttpBasicAuthenticationFactory.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StephBug\Firewall\Factory\Authentication;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Hashing\Hasher;
+use StephBug\Firewall\Factory\Contracts\AuthenticationServiceFactory;
+use StephBug\Firewall\Factory\Payload\PayloadFactory;
+use StephBug\Firewall\Factory\Payload\PayloadService;
+use StephBug\SecurityModel\Application\Http\Firewall\HttpBasicAuthenticationFirewall;
+use StephBug\SecurityModel\Guard\Authentication\Providers\IdentifierPasswordAuthenticationProvider;
+use StephBug\SecurityModel\Guard\Guard;
+use StephBug\SecurityModel\User\UserChecker;
+use Symfony\Component\HttpFoundation\RequestMatcherInterface;
+
+class HttpBasicAuthenticationFactory implements AuthenticationServiceFactory
+{
+    /**
+     * @var Application
+     */
+    private $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    public function create(PayloadService $payload): PayloadFactory
+    {
+        return (new PayloadFactory())
+            ->setFirewall($this->registerFirewall($payload))
+            ->setProvider($this->registerProvider($payload));
+    }
+
+    protected function registerFirewall(PayloadService $payload): string
+    {
+        $id = 'firewall.basic_firewall.' . $payload->securityKey->value();
+
+        $this->app->bind($id, function (Application $app) use ($payload) {
+            return new HttpBasicAuthenticationFirewall(
+                $app->make(Guard::class),
+                $this->matcher(),
+                $app->make($payload->context->entrypointId()),
+                $payload->securityKey
+            );
+        });
+
+        return $id;
+    }
+
+    protected function registerProvider(PayloadService $payload): string
+    {
+        $id = 'firewall.generic_provider.' . $payload->securityKey->value();
+
+        $this->app->bind($id, function (Application $app) use ($payload) {
+            return new IdentifierPasswordAuthenticationProvider(
+                $app->make($payload->userProviderId),
+                $app->make(UserChecker::class),
+                $payload->securityKey,
+                $app->make(Hasher::class)
+            );
+        });
+
+        return $id;
+    }
+
+    public function position(): string
+    {
+        return 'http';
+    }
+
+    public function matcher(): ?RequestMatcherInterface
+    {
+        return new HttpBasicAuthenticationFactory();
+    }
+
+    public function userProviderKey(): ?string
+    {
+        return null;
+    }
+
+    public function serviceKey(): string
+    {
+        return 'http-basic';
+    }
+}
