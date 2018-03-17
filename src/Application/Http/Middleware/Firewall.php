593ab8c1a8c748f1e09881e477c54041d552a9d7
steph bug <hewsda1@gmail.com>
2018-03-17 17:28:29 +0100

    redesign firewall middleware

--- src/Application/Http/Middleware/Firewall.php
+++ src/Application/Http/Middleware/Firewall.php
@@ -4,46 +4,27 @@ declare(strict_types=1);
 
 namespace StephBug\Firewall\Application\Http\Middleware;
 
-use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Events\RouteMatched;
-use Illuminate\Routing\Route;
-use Illuminate\Routing\Router;
-use Illuminate\Support\Collection;
-use StephBug\Firewall\Application\Exception\DebugFirewall;
-use StephBug\Firewall\Factory\Contracts\FirewallExceptionRegistry;
-use StephBug\Firewall\Factory\Factory;
+use StephBug\Firewall\ServiceRegistry;
 
 class Firewall
 {
     /**
-     * @var Factory
+     * @var ServiceRegistry
      */
-    private $factory;
-
-    /**
-     * @var Router
-     */
-    private $router;
+    private $registry;
 
     /**
      * @var Dispatcher
      */
     private $eventDispatcher;
 
-    /**
-     * @var Application
-     */
-    private $app;
-
-    public function __construct(Factory $factory, Router $router, Dispatcher $eventDispatcher, Application $app)
+    public function __construct(ServiceRegistry $registry, Dispatcher $eventDispatcher)
     {
-        $this->factory = $factory;
-        $this->router = $router;
+        $this->registry = $registry;
         $this->eventDispatcher = $eventDispatcher;
-        $this->app = $app;
     }
 
     public function handle(Request $request, \Closure $next)
@@ -53,35 +34,8 @@ class Firewall
         return $next($request);
     }
 
-    protected function processFirewall(Request $request, Route $route): void
-    {
-        $this->factory
-            ->raise(new Collection($route->middleware()), $request)
-            ->each(function (array $middleware, string $name) {
-                $middleware = $this->setExceptionHandler($middleware, $name);
-
-                $this->router->middlewareGroup($name, $middleware);
-            });
-    }
-
-    protected function setExceptionHandler(array $middleware, string $name): array
-    {
-        $exception = $this->app->make(array_pop($middleware));
-
-        if (!$exception instanceof DebugFirewall) {
-            throw new \RuntimeException(
-                sprintf('Last middleware of firewall %s must implement %s interface',
-                    $name, DebugFirewall::class)
-            );
-        }
-
-        $this->app->make(ExceptionHandler::class)->setFirewallHandler($exception);
-
-        return $middleware;
-    }
-
     public function onEvent(RouteMatched $event): void
     {
-        $this->processFirewall($event->request, $event->route);
+        $this->registry->register($event->request, $event->route);
     }
 }
---
+++ src/ServiceRegistry.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace StephBug\Firewall;
+
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Collection;
+use StephBug\Firewall\Application\Exception\DebugFirewall;
+use StephBug\Firewall\Factory\Factory;
+
+class ServiceRegistry
+{
+    /**
+     * @var Application
+     */
+    private $app;
+
+    /**
+     * @var Factory
+     */
+    private $factory;
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    public function __construct(Application $app, Factory $factory, Router $router)
+    {
+        $this->app = $app;
+        $this->factory = $factory;
+        $this->router = $router;
+    }
+
+    public function register(Request $request, Route $route): void
+    {
+        $this->factory
+            ->raise(new Collection($route->middleware()), $request)
+            ->each(function (array $middleware, string $name) {
+                $middleware = $this->setExceptionHandler($middleware, $name);
+
+                $this->router->middlewareGroup($name, $middleware);
+            });
+    }
+
+    protected function setExceptionHandler(array $middleware, string $name): array
+    {
+        $exception = $this->app->make(array_pop($middleware));
+
+        if (!$exception instanceof DebugFirewall) {
+            throw new \RuntimeException(
+                sprintf('Last middleware of firewall %s must implement %s interface',
+                    $name, DebugFirewall::class)
+            );
+        }
+
+        $this->app->make(ExceptionHandler::class)->setFirewallHandler($exception);
+
+        return $middleware;
+    }
+}
