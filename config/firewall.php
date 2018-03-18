dd9c88184bfe6df772f6ee97050339d8f2afc071
steph bug <hewsda1@gmail.com>
2018-03-18 12:34:11 +0100

    reset switch user default bootstrap after anonymous bootstrap

--- config/firewall.php
+++ config/firewall.php
@@ -13,8 +13,8 @@ return [
         \StephBug\Firewall\Factory\Bootstrap\LogoutService::class,
         \StephBug\Firewall\Factory\Bootstrap\SerializationContext::class,
         \StephBug\Firewall\Factory\Bootstrap\AuthenticationService::class,
-        \StephBug\Firewall\Factory\Bootstrap\ImpersonateUser::class,
         \StephBug\Firewall\Factory\Bootstrap\AnonymousRequest::class,
+        \StephBug\Firewall\Factory\Bootstrap\ImpersonateUser::class,
         \StephBug\Firewall\Factory\Bootstrap\AccessControl::class,
         \StephBug\Firewall\Factory\Bootstrap\FirewallExceptionHandler::class
     ],
